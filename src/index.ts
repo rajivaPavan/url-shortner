a7f117421edaf548a6b98257302d6ee23ff184bc
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-12-14 23:04:06 +0530

    Update index.ts and wrangler.toml

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,26 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+// src/index.ts
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-}
-
-export default {
+	SHORT_URLS: KVNamespace;
+  }
+  
+  export default {
 	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
+	  request: Request,
+	  env: Env,
+	  _ctx: ExecutionContext
 	): Promise<Response> {
-		return new Response("Hello World!");
+	  const url = new URL(request.url);
+  
+	  const { pathname } = url;
+  
+	  const redirectURL = await env.SHORT_URLS.get(pathname);
+  
+	  if (!redirectURL) {
+		return new Response(
+		  `There is no defined URL for the path: '${pathname}', sorry :(`
+		);
+	  }
+  
+	  return Response.redirect(redirectURL, 301);
 	},
-};
+  };
